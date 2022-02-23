0f66ceb90520609aa5d19a33ac490b584d442c2a
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-02-23 15:47:42 +0100

    Update ft_push.c

--- srcs/movements/ft_push.c
+++ srcs/movements/ft_push.c
@@ -12,6 +12,32 @@
 
 #include "../incs/push_swap.h"
 
+/*
+  NAME
+  ****
+  ft_push_a ---> push node to stack b
+  
+  SYNOPSIS
+  ***********
+  #include "../incs/push_swap.h"
+  int	ft_push_a(t_stack_a **head_a, t_stack_b **head_b);
+  
+  DESCRIPTION
+  ***********
+  The function ft_push_a() takes the first node from stack b and puts it on top
+  of stack a. If stack b is empty, does nothing.
+  
+  PARAMETERS
+  **********
+  head_a ---> The head node from stack a
+  head_b ---> The head node from stack b
+  
+  RETURN VALUE
+  ************
+  Void function so it returns nothing. The function recieves as parameters the
+  memory addresses of the main nodes, so it modifies them directly.
+*/
+
 void	ft_push_a(t_stack_a **head_a, t_stack_b **head_b)
 {
 	t_stack_b	*tmp;
@@ -23,6 +49,32 @@ void	ft_push_a(t_stack_a **head_a, t_stack_b **head_b)
 	ft_insert_nbr_a(tmp1->nbr_b, 1, head_a);
 	printf("pa\n");
 }
+
+/*
+  NAME
+  ****
+  ft_push_b ---> push node to stack a
+  
+  SYNOPSIS
+  ***********
+  #include "../incs/push_swap.h"
+  int	ft_push_b(t_stack_a **head_a, t_stack_b **head_b);
+  
+  DESCRIPTION
+  ***********
+  The function ft_push_b() takes the first node from stack a and puts it on top
+  of stack b. If stack a is empty, does nothing.
+  
+  PARAMETERS
+  **********
+  head_a ---> The head node from stack a
+  head_b ---> The head node from stack b
+  
+  RETURN VALUE
+  ************
+  Void function so it returns nothing. The function recieves as parameters the
+  memory addresses of the main nodes, so it modifies them directly.
+*/
 
 void	ft_push_b(t_stack_a **head_a, t_stack_b **head_b)
 {
