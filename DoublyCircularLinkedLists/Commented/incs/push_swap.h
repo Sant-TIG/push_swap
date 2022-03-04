43205cd4bc803ca6af8cd058a9e0231765fa3709
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-03-04 16:48:20 +0100

    Create push_swap.h

---
+++ DoublyCircularLinkedLists/Commented/incs/push_swap.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sperez-p <sperez-p@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 14:49:13 by sperez-p          #+#    #+#             */
+/*   Updated: 2022/03/03 20:47:11 by sperez-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+	int				nbr;
+	struct s_stack	*prev;
+	struct s_stack	*next;
+}	t_stack;
+
+typedef struct s_pushswap
+{
+	size_t	stacka_len;
+	size_t	stackb_len;
+}	t_pushswap;
+
+/* UTILS FUNCTIONS */
+
+char	**ft_split(const char *str, char c);
+size_t	ft_strlen(const char *str);
+int	ft_digit_str(char *str);
+int	ft_isdigit(int c);
+
+#endif
