335714801e4325f334ff99677208cad3c4b224d4
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-03-14 14:39:05 +0100

    Update push_swap.h

--- DoublyCircularLinkedLists/NoCommented/NoCommented/incs/push_swap.h
+++ DoublyCircularLinkedLists/NoCommented/NoCommented/incs/push_swap.h
@@ -26,14 +26,44 @@ typedef struct s_stack
 
 typedef struct s_pushswap
 {
-	size_t	stacka_len;
-	size_t	stackb_len;
+	int	stacka_len;
+	int	stackb_len;
 }	t_pushswap;
+
+/* CHECK FUNCTIONS */
+
+int	ft_check_list(t_stack *stack_a, t_pushswap *utils);
+int	ft_check_sorted(t_stack *stack_a);
+int	ft_check_args(char **args);
+
+/* MOVEMENTS FUNCTIONS */
+
+void	ft_push_a(t_stack **stack_a, t_stack **stack_b, t_pushswap **utils);
+void	ft_push_b(t_stack **stack_a, t_stack **stack_b, t_pushswap **utils);
+void	ft_rotate_a(t_stack **stack_a);
+void	ft_rotate_b(t_stack **stack_b);
+void	ft_rotate_ab(t_stack **stack_a, t_stack **stack_b);
+void	ft_reverse_rotate_a(t_stack **stack_a);
+void	ft_reverse_rotate_b(t_stack **stack_b);
+void	ft_reverse_rotate_ab(t_stack **stack_a, t_stack **stack_b);
+void	ft_swap_a(t_stack **stack_a);
+void	ft_swap_b(t_stack **stack_b);
+void	ft_swap_ab(t_stack **stack_a, t_stack **stack_b);
 
 /* LINKED FUNCTIONS */
 
-size_t	ft_listlen(t_stack *start);
-void    ft_insert_nbr(int nbr, int pos, t_stack **stack_a);
+int	ft_quick_sort(t_stack **stack_a, int start, int end);
+int		ft_listlen(t_stack *stack_a);
+//void	ft_insert_nbr(int nbr, int pos, t_stack **stack_a);
+void	ft_insert_begin(int nbr, t_stack **stack);
+void	ft_insert_empty(int nbr, t_stack **stack);
+void	ft_insert_end(int nbr, t_stack **stack);
+void	ft_insert_nbr(int nbr, int pos, t_stack **stack, int stack_len);
+
+/* PRINT FUNCTIONS */
+
+void	ft_print_args(char **args);
+void	ft_print_list(t_stack *stack_a);
 
 /* UTILS FUNCTIONS */
 
