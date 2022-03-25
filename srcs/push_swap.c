15294cfb53d4dfd41d4bc0538728193e8995e671
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-03-25 12:20:43 +0100

    Add files via upload

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -46,11 +46,14 @@ static int	ft_treat_args(char **args, t_stacks *stacks)
 	if (!ft_check_list(stacks->stack_a, stacks->utils))
 	{
 		ft_free_list(&stacks->stack_a, stacks->utils->stacka_len);
-		write(1, "Error\n", 7);
+		write(1, "Error\n", 6);
 		return (0);
 	}
 	if (!ft_check_sorted(stacks->stack_a))
+	{
+		ft_free_list(&stacks->stack_a, stacks->utils->stacka_len);
 		return (0);
+	}
 	return (1);
 }
 
@@ -70,11 +73,11 @@ static void	ft_treat_case_one(char *argv, t_stacks *stacks)
 	if (!ft_treat_args(args, stacks))
 	{
 		ft_free_args(args);
-		free(&stacks->utils);
 		return ;
 	}
 	ft_free_args(args);
 	ft_control_sort(stacks);
+	ft_free_list(&stacks->stack_a, stacks->utils->stacka_len);
 }
 
 int	main(int argc, char **argv)
@@ -96,9 +99,10 @@ int	main(int argc, char **argv)
 				return (0);
 			}
 			ft_control_sort(&stacks);
+			ft_free_list(&stacks.stack_a, stacks.utils->stacka_len);
 		}
 	}
-	ft_free_list(&stacks.stack_a, stacks.utils->stacka_len);
+	//ft_free_list(&stacks.stack_a, stacks.utils->stacka_len);
 	free(stacks.utils);
 	return (0);
 }
